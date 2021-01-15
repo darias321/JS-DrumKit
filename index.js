c9091f3d9e80033697d9661dbc1d0c96bf992c2d
Danielle Arias <daniellearias321@gmail.com>
2021-01-15 15:17:34 -0800

    added event lister to keys and added sound files

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hello there"); 
+window.addEventListener("keydown", function (e) {
+const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+//this will stop function all together when a key that isn't available is hit. 
+if(!audio) return; 
+  });
