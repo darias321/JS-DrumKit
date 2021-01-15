a9deeb47b827f1d579cc86f352e3f3890fe2fafb
Danielle Arias <daniellearias321@gmail.com>
2021-01-15 15:58:49 -0800

    created animation when key hit and added transition function

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-window.addEventListener("keydown", function (e) {
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-//this will stop function all together when a key that isn't available is hit. 
-if(!audio) return; 
+function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    
+    //this will stop function all together when a key that isn't available is hit. 
+    if(!audio) return; 
+    
+    // allows key to be pressed over and over
+    audio.currentTime = 0; 
+    audio.play();  
+    
+    // starts animation when key is hit 
+    key.classList.add("playing"); 
+      };
 
-audio.play(); 
-  });
+// function for creating transition of keys anitmation when hit 
+  function removeTransition(e){
+
+// skip if it's not a transform
+if(e.propertyName !== 'transform') return; 
+
+//removes event so anitmation transitions 
+this.classList.remove('playing');  
+  }
+
+  const keys = document.querySelectorAll(".key"); 
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+  window.addEventListener("keydown", playSound); 
